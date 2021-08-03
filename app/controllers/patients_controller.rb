b3f6fe6487dc94c83e38db4afd915755696ca9b0
drewsview34 <drewsview34@outlook.com>
2021-08-03 15:01:17 -0700

    added actions to the controllers

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,35 @@
 class PatientsController < ApplicationController
+
+    def index
+        @patients = Patient.all
+    end
+
+    def new
+        @patient = Patient.new(patient_params)
+    end
+
+    def create
+        @patient.create(patient_params)
+        redirect_to patients_path
+    end
+
+    def edit
+        @doctor = Doctor.find(params[:id])
+    end
+
+    def show
+        @doctor = Doctor.find(params[:id])
+    end
+
+    # def update
+
+    # end
+
+
+    private
+
+    def patient_params
+        params.require(:patients).permit(:name, :age)
+    end
+
 end
